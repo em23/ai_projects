c87b825c9dd3511d14433415ac587cb7eb87a2cb
em23 <enea.mano@yahoo.com>
2019-01-19 19:36:18 -0700

    Done with question 8.

--- p1/search/searchAgents.py
+++ p1/search/searchAgents.py
@@ -545,6 +545,8 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
+        return search.breadthFirstSearch(problem)
+
         util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -581,6 +583,8 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
+        return  self.food[x][y]
+
         util.raiseNotDefined()
 
 def mazeDistance(point1, point2, gameState):
